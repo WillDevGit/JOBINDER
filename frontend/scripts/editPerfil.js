f7b89cef29e112445b74c6a933e338efc366701c
Willian Silva <williansilvaws063@gmail.com>
2024-10-14 18:42:50 -0300

    Adiciona opções de edição

--- frontend/scripts/editPerfil.js
+++ frontend/scripts/editPerfil.js
@@ -1,15 +1,33 @@
 import { getUser, userLogged, updateUser, getUserData } from "../../backend/user.js";
 
+// Pegar id dos elementos
 const user = userLogged();
 const usuarioDados = getUserData(userLogged()); 
-const editarImg = document.getElementById("imagem"); 
-const editarnome = document.getElementById("nomeCard"); 
-const editarDesc = document.getElementById("descricao"); 
+const imagem = document.getElementById("imagem"); 
+const nome = document.getElementById("nomeCard"); 
+const descricao = document.getElementById("descricao"); 
+const nomeButtonEdit = document.getElementById("editNome"); 
+const descButtonEdit = document.getElementById("editDesc"); 
+const opcoesDiv = document.getElementById("opcoes"); 
 
-editarImg.style.backgroundImage = `url(${usuarioDados.serviceProfile.serviceImg})`;
-editarnome.innerHTML = usuarioDados.fullName; 
-editarDesc.innerHTML = usuarioDados.serviceProfile.services; 
+// Adiciona dados do perfil do usuario no card 
+imagem.style.backgroundImage = `url(${usuarioDados.serviceProfile.serviceImg})`;
+nome.innerHTML = usuarioDados.fullName; 
+descricao.innerHTML = usuarioDados.serviceProfile.services; 
+
+// 
+nomeButtonEdit.addEventListener("click" , () => {
+    const editarNome = document.getElementById("interfaceNome"); 
+    opcoesDiv.style.display = "none";
+    editarNome.style.display = "block"; 
+
+}); 
+
+descButtonEdit.addEventListener("click" , () => {
+    const editarDesc = document.getElementById("interfaceDesc"); 
+    opcoesDiv.style.display = "none";
+    editarDesc.style.display = "block"; 
+}); 
 
 
 
-console.log(usuarioDados)
